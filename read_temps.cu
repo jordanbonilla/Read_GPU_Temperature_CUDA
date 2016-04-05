b44f1f30d38db10790b1a3051bc706789c6be157
Jordan Bonilla <jbonilla@caltech.edu>
2016-04-04 23:34:07 -0700

    Update read_temps.cu

--- read_temps.cu
+++ read_temps.cu
@@ -18,57 +18,59 @@
 //--------------------------------------------------------------------------
 
 #include <cstdio> // printf
-#include <cstring> // memcpy
 #include <stdlib.h> // popen, pclose, atoi, fread
 #include <cuda_runtime.h> // cudaGetDeviceCount
 
 // Read temperatures of all connected NVIDIA GPUs
 void read_temps() 
 {
-	// Get the number of GPUs on this machine
-	int num_devices;
-	cudaGetDeviceCount(&num_devices);
-	if(num_devices == 0) {
-	  printf("No NVIDIA GPUs detected\n");
-		return;
-	}
-	// Read GPU info into buffer "output" using the "nvidia-smi" command
-	const unsigned int MAX_BYTES = 10000;
-	const unsigned int MAX_DIGITS_PER_INT = 10;
-	char output[MAX_BYTES];
-	FILE *fp = popen("nvidia-smi &> /dev/null", "r");
-	fread(output, sizeof(char), MAX_BYTES, fp);
-	pclose(fp);
-	// array to hold GPU temperatures
-	int * temperatures = new int[num_devices];
-	// parse output for temperatures using knowledge of "nvidia-smi" output format
-	int i = 0;
-	unsigned int num_temps_parsed = 0;
-	while(output[i] != '\0') {
-		if(output[i] == '%') {
-			unsigned int temp_begin = i + 1;
-			while(output[i] != 'C') {
-				++i;
-			}
-			unsigned int temp_end = i;
-			char this_temperature[MAX_DIGITS_PER_INT];
-			memcpy(this_temperature, output + temp_begin, temp_end);
-			temperatures[num_temps_parsed] = atoi(this_temperature);
-			num_temps_parsed++;
-		}
-		++i;
-	}
-	for (int i = 0; i < num_devices; i++) 
-	{
-	  printf("GPU %d temperature: %d C\n", i, temperatures[i]);
-	}
+    // Get the number of GPUs on this machine
+    int num_devices;
+    cudaGetDeviceCount(&num_devices);
+    if(num_devices == 0) {
+      printf("No NVIDIA GPUs detected\n");
+        return;
+    }
+    // Read GPU info into buffer "output" using the "nvidia-smi" command
+    const unsigned int MAX_BYTES = 10000;
+    char output[MAX_BYTES];
+    FILE *fp = popen("nvidia-smi &> /dev/null", "r");
+    fread(output, sizeof(char), MAX_BYTES, fp);
+    pclose(fp);
+    // array to hold GPU temperatures
+    int * temperatures = new int[num_devices];
+    // parse output for temperatures using knowledge of "nvidia-smi" output format
+    int i = 0;
+    unsigned int num_temps_parsed = 0;
+    while(output[i] != '\0') {
+        if(output[i] == '%') {
+            unsigned int temp_begin = i + 1;
+            while(output[i] != 'C') {
+                 ++i;
+            }
+            unsigned int temp_end = i;
+            char this_temperature[32];
+                        // Read in the characters cooresponding to this temperature
+                        for(int j = 0; j < temp_end - temp_begin; ++j) {
+                            this_temperature[j] = output[temp_begin + j];
+                        }
+                        this_temperature[temp_end - temp_begin + 1] = '\0';
+            temperatures[num_temps_parsed] = atoi(this_temperature);
+            num_temps_parsed++;
+        }
+        ++i;
+    }
+    for (int i = 0; i < num_devices; i++) 
+    {
+      printf("GPU %d temperature: %d C\n", i, temperatures[i]);
+    }
 
-	// Free memory and return
-	delete(temperatures);
-	return;
+    // Free memory and return
+    delete(temperatures);
+    return;
 }
 
 int main(int argc, char **argv) {
-	read_temps();
-	return 0;
+    read_temps();
+    return 0;
 }
